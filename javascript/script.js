4e3504f80cbae07cf2ba75d19ade12acd86d4057
Nathaniel Ryan Mathew <nathanielmat2@gmail.com>
2018-04-29 10:05:50 +0530

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -1,4 +1,5 @@
 var place;
+var url = 'https://api.openweathermap.org/data/2.5/weather?q=' + place + '&APPID=e14b4cb410b59c24981c46fe3e32bfc7&units=metric';
 function search(){
 	place = document.getElementById("go").value;
 	output();
@@ -7,3 +8,13 @@ function search(){
 function output(){
 	document.getElementById("place").innerHTML = place;
 }
+fetch(url)
+      .then(  
+		function(response) {  
+                return response.json();
+                })
+
+            .then(function(data){
+	
+
+	
