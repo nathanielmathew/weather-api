2602d1ac04c73e9705742149f875c564047b9cfb
Nathaniel <nathanielmat2@gmail.com>
2018-04-29 13:02:30 +0530

    updated

--- javascript/script.js
+++ javascript/script.js
@@ -1,25 +1,24 @@
 var place;
-var url = 'https://api.openweathermap.org/data/2.5/weather?q=' + place + '&APPID=e14b4cb410b59c24981c46fe3e32bfc7&units=metric';
+
 function search(){
 	place = document.getElementById("go").value;
-	output();
-}
-
-function output(){
-	document.getElementById("place").innerHTML = place;
-}
-fetch(url)
+	var url = 'https://api.openweathermap.org/data/2.5/weather?q=' + place + '&APPID=e14b4cb410b59c24981c46fe3e32bfc7&units=metric';
+	fetch(url)
       .then(  
 		function(response) {  
                 return response.json();
 			
                 })
 
-            .then(function(data){ document.getElementById("weather").innerHTML=data.coord.weather;
-	
-document.getElementById("maxtemp").innerHTML=data.main.temp_max;,
-	document.getElementById("mintemp").innerHTML=data.main.temp_min;     });
+            .then(function(data){ 
+            		document.getElementById("place").innerHTML = place;
+					document.getElementById("weather").innerHTML= data.weather[0].description;
+					document.getElementById("maxtemp").innerHTML=data.main.temp_max;
+					document.getElementById("mintemp").innerHTML=data.main.temp_min; 
+			});
         } 
+
+
 	
 	
 	
